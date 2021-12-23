62443c246938553880b83620aeb1b4f9a1b5e9b1
Pietro222222 <pietro.lima.queiroz@gmail.com>
2021-12-23 08:18:24 -0300

    better var naming

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,21 @@ fn main() {
         match d.event_type {
             EventType::MouseMove { x, y } => {
                 std::thread::sleep(Duration::from_millis(1));
-                let mut mouse_position_h = (x / w as f64) as usize;
-                let mut mouse_position_w = (y / h as f64) as usize;
-                let mut screen = vec![vec![""; w as usize]; h as usize];
-                if mouse_position_w >= screen.len() {
-                    mouse_position_w = screen.len() - 1;
+                let mut mouse_position_h = (y / h as f64) as usize;
+                let mut mouse_position_w = (x / w as f64) as usize;
+                let mut screen = vec![vec![" "; w as usize]; h as usize];
+                if mouse_position_w >= screen[0].len() {
+                    mouse_position_w = screen[0].len() - 1;
                 }
-                if mouse_position_h >= h as usize {
-                    mouse_position_h = h as usize - 1;
+                if mouse_position_h >= screen.len() {
+                    mouse_position_h = screen.len() - 1;
                 }
-                screen[mouse_position_w as usize][mouse_position_h as usize] = "X";
+
+                println!("{} {}", mouse_position_h, mouse_position_w);
+                screen[mouse_position_h as usize][mouse_position_w as usize] = "\x1b[93mX\x1b[0m";
                 print!("\x1B[2J\x1B[1;1H");
                 for i in screen {
-                    println!("{}", i.join(" ").as_str());
+                    println!("{}", i.join("").as_str());
                 }
             }
             _ => {}
